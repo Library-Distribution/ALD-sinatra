8a2dea96bca75a6ad3682cfa5f3c64056cc798d8
maul.esel <maul.esel@go4more.de>
2014-03-16 16:02:22 +0100

    implement proper find for IdRecord

--- models.rb
+++ models.rb
@@ -2,6 +2,16 @@ module IdRecord
   def id
     super.unpack('H*').first.upcase
   end
+
+  def self.included(o)
+    o.extend(ClassMethods)
+  end
+
+  module ClassMethods
+    def find(id)
+      find_by "id = E'\\\\x#{id}'"
+    end
+  end
 end
 
 class Item < ActiveRecord::Base
--- routes/users.rb
+++ routes/users.rb
@@ -4,11 +4,11 @@ get '/users/list' do
 end
 
 get %r{/users/describe/([0-9a-fA-F]{32})} do |id|
-  user = User.find_by "id = E'\\\\x#{id}'"
+  user = User.find(id)
   describe_user user
 end
 
 get %r{/users/describe/(.+)} do |name|
-  user = User.find_by_name name
+  user = User.find_by_name(name)
   describe_user user
 end
