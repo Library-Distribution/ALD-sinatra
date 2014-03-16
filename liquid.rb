5cd68534dc8be7045c9399fc1964ef9e0f5eff4f
maul.esel <maul.esel@go4more.de>
2014-03-16 23:42:02 +0100

    simplify json filter

--- liquid.rb
+++ liquid.rb
@@ -3,7 +3,7 @@ require 'json'
 module ALD
   module LiquidExtensions
     def json(object)
-      object.reject {|k,v| k == "collections" }.to_json
+      object.to_json
     end
 
     def filter(object, *keys)
