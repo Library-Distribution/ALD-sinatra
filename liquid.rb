5d18f8b043a2191dbf66f9439aef8dffde5106ae
maul.esel <maul.esel@go4more.de>
2014-03-24 23:20:51 +0100

    add Liquid filters #parse_json and #merge

--- liquid.rb
+++ liquid.rb
@@ -6,6 +6,17 @@ module ALD
       object.to_json
     end
 
+    def parse_json(str)
+      JSON.parse(str)
+    end
+
+    def merge(hsh, *others)
+      others.each do |other|
+        hsh.merge!(other)
+      end
+      hsh
+    end
+
     def filter(object, *keys)
       SinatraHelpers::Templates.filter(object, keys)
     end
